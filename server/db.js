8020c0503f49853c341942606023e3d5112527a0
Zoheb Alli Khan <understandingzak@gmail.com>
2021-05-05 11:02:10 +0530

    Hide port Number

--- server/db.js
+++ server/db.js
@@ -4,13 +4,13 @@ require('dotenv').config();
 
 var user = process.env.USER;
 var pass = process.env.PASS;
-
+var p = process.env.PORT;
 
 const pool = new Pool({
     user: user,
     password: pass,
     host: "localhost",
-    port: "5432",
+    port: p,
     database: "docs"
 })
 
