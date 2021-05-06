4836fbee92edf455e75e46a92b492b778a1f8fcc
Zoheb Alli Khan <understandingzak@gmail.com>
2021-05-06 13:37:54 +0530

    Components Added

---
+++ client/src/components/InputNote.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function InputNote() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default InputNote
---
+++ client/src/components/ListNote.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ListNote() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ListNote
